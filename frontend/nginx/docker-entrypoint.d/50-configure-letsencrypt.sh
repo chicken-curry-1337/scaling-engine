66b0faa6ed8e7f4e5c1d186e891a1118eaca4ccd
Глеб Белкин <ga_belkin@betboom.org>
2025-11-10 19:25:14 +0300

    fix

--- frontend/nginx/docker-entrypoint.d/50-configure-letsencrypt.sh
+++ frontend/nginx/docker-entrypoint.d/50-configure-letsencrypt.sh
@@ -54,6 +54,19 @@ server {
     }
 
     location / {
+
+        if ($request_method = OPTIONS) {
+            add_header Access-Control-Allow-Origin "*";
+            add_header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE, OPTIONS";
+            add_header Access-Control-Allow-Headers "Content-Type, Authorization";
+            add_header Content-Length 0;
+            add_header Content-Type text/plain;
+            return 204;
+        }
+
+        add_header Access-Control-Allow-Origin "*" always;
+        add_header Access-Control-Allow-Methods "GET, POST, PUT, PATCH, DELETE, OPTIONS" always;
+        add_header Access-Control-Allow-Headers "Content-Type, Authorization" always;
         proxy_set_header Host \$host;
         proxy_set_header X-Real-IP \$remote_addr;
         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
